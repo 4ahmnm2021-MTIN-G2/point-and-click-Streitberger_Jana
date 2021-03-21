0b83711dc1b2ee83d678a19b064cffff76a55446
jstreitberger <jana.streitberger03@htl-salzburg.ac.at>
2021-03-21 19:55:09 +0100

    created sprite

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Transform thisTransform;
+    public Vector3 mousePos;
+    public Vector3 worldPos;
+    public Vector3 playerPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousePos = Input.mousePosition;
+        worldPos = Camera.main.ScreenToWorldPoint(mousePos);
+        playerPos = new Vector3(worldPos.x, worldPos.y, thisTransform.position.z);
+        thisTransform.position = playerPos;
+    }
+}
