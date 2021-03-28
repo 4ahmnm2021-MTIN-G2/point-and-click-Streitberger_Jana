21831b8c30628edd492c609f5e1e4f43045af10c
jstreitberger <jana.streitberger03@htl-salzburg.ac.at>
2021-03-28 12:53:15 +0200

    Player Movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,22 +4,29 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-  
+    public Transform thisTransform;
     public Vector3 mousePos;
     public Vector3 worldPos;
+    public Vector3 playerPos;
+    public float speed = 0.1f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerPos = thisTransform.position; 
     }
 
     // Update is called once per frame
     void Update()
     {
-        mousePos = Input.mousePosition;
-        worldPos = Camera.main.ScreenToWorldPoint(mousePos);
-        this.transform.position = worldPos;
+        if (Input.GetMouseButtonDown(0))
+        {
+            mousePos = Input.mousePosition;
+            worldPos = Camera.main.ScreenToWorldPoint(mousePos);
+            playerPos = new Vector3(worldPos.x, worldPos.y, thisTransform.position.z);
+        }
+
+        thisTransform.position = Vector3.MoveTowards(thisTransform.position, playerPos, speed);
  
     }
 }
