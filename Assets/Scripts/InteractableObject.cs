3dc5a5f91eb5208a66d4c48427d93c2765ef1fc7
jstreitberger <jana.streitberger03@htl-salzburg.ac.at>
2021-04-17 19:22:36 +0200

    added InteractableObjects

---
+++ Assets/Scripts/InteractableObject.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class InteractableObject : MonoBehaviour
+{
+    public GameObject Object;
+    public Text ObjectText;
+    public string TextValue;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        Object.SetActive(false);
+        ObjectText.text = TextValue;
+    }
+
+}
