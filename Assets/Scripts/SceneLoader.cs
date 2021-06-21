be66cb6217c4b3f82657a547cafb57c6d9185f2e
jstreitberger <jana.streitberger03@htl-salzburg.ac.at>
2021-06-21 10:19:40 +0200

    add Sprites

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void NextScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+
+}
+
