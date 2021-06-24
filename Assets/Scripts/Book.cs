e96ab6fc07efe411bb09ba51ffe8dd893679628c
jstreitberger <jana.streitberger03@htl-salzburg.ac.at>
2021-06-24 17:11:21 +0200

    added CounterScript

---
+++ Assets/Scripts/Book.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Book : MonoBehaviour
+{
+    public GameObject bookOpen;
+        
+        public void OnMouseDown()
+    {
+        bookOpen.SetActive(true);
+        StartCoroutine(Reading());
+
+        IEnumerator Reading()
+        {
+            yield return new WaitForSeconds(3);
+            bookOpen.SetActive(false);
+        }
+      
+    }
+}
---
+++ Assets/Scripts/Counter.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Counter : MonoBehaviour
+{
+    public int counter = 0;
+    public GameObject button;
+    public GameObject gameOver;
+    public GameObject items;
+    public int falseCounter = 0;
+
+
+    public void OnMouseDown()
+    {
+        if (gameObject.CompareTag("Zutat")){
+            Debug.Log("wuerde funk");
+            counter +=1;
+        }
+        //geht nicht wegen Manager CompareTag
+        if(gameObject.CompareTag("Falsch"))
+        {
+            Debug.Log("falsch geht");
+            falseCounter += 1;
+            counter -= 1;
+        }
+
+        if(counter == 6)
+        {
+            button.SetActive(true);
+        }
+        if(falseCounter >= 1)
+        {
+            gameOver.SetActive(true);
+        }
+    }
+
+
+
+}
